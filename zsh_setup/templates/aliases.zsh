4445301d2167ccd39f17476b22a61d306d280d56
MurphyYi <zy84338719@hotmail.com>
2026-08-05 08:57:20 +0800

    feat(zsh-setup): add template files

--- zsh_setup/templates/aliases.zsh
+++ zsh_setup/templates/aliases.zsh
@@ -1,5 +1,44 @@
-# zsh_setup/templates/aliases.zsh
-# 通用别名配置模板
-#
+# ~/.config/zsh_setup/templates/aliases.zsh
+# 通用别名模板
 
-# TODO: 别名配置内容（Task 9）
+# 常用别名
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# 安全操作
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# 快捷命令
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Git 别名
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gl='git log --oneline --graph'
+alias gd='git diff'
+
+# Docker 别名
+alias dk='docker'
+alias dkc='docker compose'
+alias dkps='docker ps'
+alias dki='docker images'
+
+# 系统信息
+alias meminfo='free -h'
+alias cpuinfo='lscpu'
+alias diskinfo='df -h'
+
+# 开发工具
+alias py='python3'
+alias pip='pip3'
+alias serve='python3 -m http.server'
--- zsh_setup/templates/env.zsh
+++ zsh_setup/templates/env.zsh
@@ -1,5 +1,36 @@
-# zsh_setup/templates/env.zsh
-# 环境变量配置模板
-#
+# ~/.config/zsh_setup/templates/env.zsh
+# 环境变量模板
 
-# TODO: 环境变量配置内容（Task 9）
+# 默认编辑器
+export EDITOR='vim'
+export VISUAL='vim'
+
+# 语言环境
+export LANG='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+
+# Go 环境
+export GOPATH="$HOME/go"
+export PATH="$GOPATH/bin:$PATH"
+
+# Node.js 环境
+export NODE_PATH="$HOME/.node_modules"
+export PATH="$NODE_PATH/bin:$PATH"
+
+# Python 环境
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONUNBUFFERED=1
+
+# Rust 环境
+export CARGO_HOME="$HOME/.cargo"
+export RUSTUP_HOME="$HOME/.rustup"
+export PATH="$CARGO_HOME/bin:$PATH"
+
+# Homebrew (macOS)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export HOMEBREW_NO_AUTO_UPDATE=1
+fi
+
+# 自定义 PATH
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
